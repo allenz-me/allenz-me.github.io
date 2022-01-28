629001170c803f9df0a30294522e1ad211f8e4af
zhong zijun <allenz.me@qq.com>
2022-01-28 21:11:41 +0800

    version 1

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,11 @@
-rm -rf ./docs
+# 一般情况下都没有 docs 文件夹
+
+commit_id=`git log -1 --pretty=format:%h`
 python div.py
 hugo --minify
 echo "notes.allenz.me" >> ./docs/CNAME
 git add --all
 git commit -m $1
 git push -f origin master
-echo "Succeed !"
+# echo "Succeed !"
+git reset --hard $commit_id
